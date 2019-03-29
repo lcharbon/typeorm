c70200c16ee5809ef7e89c57cb1f62b03829315b
Luc Charbonneau <luc@coastbeachwear.com>
2019-03-29 11:45:08 -0400

    Added type ORM model.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
+import "reflect-metadata";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Model } from './model';
+
+console.log(Model);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
